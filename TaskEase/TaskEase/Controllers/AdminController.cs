403311c98e9fbc6c98df0c524849b939b79e8dd3
lambsauce700 <saleh700069@gmail.com>
2023-10-01 21:39:22 +0300

    First commit

---
+++ TaskEase/TaskEase/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskEase.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Users()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Requests()
+        {
+            return View();
+        }
+    }
+}
---
+++ TaskEase/TaskEase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaskEase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
